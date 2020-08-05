8e21ef1807eb6318b58c02c11a8724c0abc1e5bf
ileshqa <ileshdarji@gmail.com>
2020-08-05 08:58:40 +0100

    Python Lists practice

---
+++ chapter3_lists/names.py
@@ -0,0 +1,19 @@
+friend_names = ['haripriya', 'kirtan', 'scooby', 'cheshta', 'bhavita']
+print(friend_names[0].title())
+print(friend_names[1].title())
+print(friend_names[2].title())
+print(friend_names[3].title())
+print(friend_names[4].title())
+
+message = f"My best friend is"
+print(f"{message}: {friend_names[0].title()}!")
+print(f"{message}: {friend_names[1].title()}!")
+print(f"{message}: {friend_names[2].title()}!")
+print(f"{message}: {friend_names[3].title()}!")
+print(f"{message}: {friend_names[4].title()}!")
+
+mode_pf_transportation = ['BMW', 'Motorbike']
+message = f"I would like to own a {mode_pf_transportation[0]} one day"
+print(message)
+message = f"I am using {mode_pf_transportation[1]} for transportation at the moment"
+print(message)
