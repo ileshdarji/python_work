3d42c3ebb4ccb7a180cc09f6c26d79864cedc4aa
ileshqa <ileshdarji@gmail.com>
2020-08-14 22:08:20 +0100

    Python Lists practice

---
+++ chapter3_lists/guest_list_exercise.py
@@ -0,0 +1,59 @@
+guests = ['dino', 'aris', 'nehal', 'premal']
+for name in guests:
+    invite_message = f"Dear {name.title()}, I invite you for dinner!"
+    print(invite_message)
+
+
+not_attending_guest = "aris"
+guests.remove(not_attending_guest)
+print(f"\nUnfortunately, {not_attending_guest.title()} cant make it :(")
+print(guests)
+
+new_guest = "rakesh"
+guests.insert(1, new_guest)
+print(f"\nInstead {new_guest.title()} is invited :)")
+print(guests)
+for name in guests:
+    invite_message = f"Dear {name.title()}, I invite you for dinner!"
+    print(invite_message)
+
+
+new_guests = ["eleshbhai", "jash", "kirtan"]
+for name in new_guests:
+    new_guest_message = f"Hey {name.title()}! I found a bigger dining table, so I will be inviting you soon! :D"
+    print(new_guest_message)
+
+guests.insert(0, new_guests[0])
+guests.insert(3, new_guests[1])
+guests.append(new_guests[2])
+print(guests)
+for name in guests:
+    invite_message = f"Dear {name.title()}, I invite you for dinner!"
+    print(invite_message)
+
+
+print("\nI can only invite two people for dinner :(")
+
+# popped_guest = guests.pop()
+# print(f"Sorry {popped_guest.title()}, I can't invite you for dinner..")
+# popped_guest = guests.pop()
+# print(f"I am sorry {popped_guest.title()}, I can't invite you for dinner..")
+# popped_guest = guests.pop()
+# print(f"I am sorry {popped_guest.title()}, I can't invite you for dinner..")
+# popped_guest = guests.pop()
+# print(f"I am sorry {popped_guest.title()}, I can't invite you for dinner..")
+# popped_guest = guests.pop()
+# print(f"I am sorry {popped_guest.title()}, I can't invite you for dinner..")
+print(len(guests))
+while len(guests) > 2:
+    popped_guest = guests.pop()
+    print(f"I am sorry {popped_guest.title()}, I can't invite you for dinner..")
+
+print(guests)
+for name in guests:
+    invite_message = f"Dear {name.title()}, You are still invited for dinner! \o/"
+    print(invite_message)
+
+del guests[0]
+del guests[0]
+print(guests)
---
+++ chapter3_lists/motorcycles.py
@@ -0,0 +1,55 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# replace the first item in the list "motorcycles"
+motorcycles.append('ducati')
+print(motorcycles)
+
+# Add a new item to the list "motorcycles" using append()
+# this will add an item at the end of the list
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding items to the empty list "motorcycles" using append()
+motorcycles = []
+print(motorcycles)
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+# Insert an item at any index to the list "motorcycles"
+# this will insert an item at desired index to the list "motorcycles"
+motorcycles.insert(1, 'ducati')
+print(motorcycles)
+
+# remove an item from the list "motorcycles" by index
+del motorcycles[0]
+print(motorcycles)
+print("***************************")
+
+
+# removing the item from list motorcycles using pop() method
+# so that you can use the popped item later
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+last_owned = motorcycles.pop()
+print(f"the last owned motorcycle was {last_owned.title()}")
+print(motorcycles)
+# pop the item by index
+first_owned = motorcycles.pop(0)
+print(f"the first owned motorcycle was {first_owned.title()}")
+print(motorcycles)
+second_owned = motorcycles.pop()
+print(f"the second owned motorcycle was {second_owned.title()}")
+print(motorcycles)
+print("***************************")
+
+# remove item by value using remove() method
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive_item = 'ducati'
+motorcycles.remove(too_expensive_item)
+print(motorcycles)
+
+print(f"\nA {too_expensive_item.title()} is very expensive for me.")
